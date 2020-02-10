556a304a433b43427ff0ddc763b07e0a7828cf56
marq-oh <58898977+marq-oh@users.noreply.github.com>
2020-02-09 17:45:44 -0800

    Initial upload; Endpoints + Models

---
+++ app.py
@@ -0,0 +1,388 @@
+import os
+import dateutil.parser
+
+
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
+from flask_cors import CORS
+
+from models import setup_db, Movies, Actors, Assignments
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+
+    @app.route('/<int:id>')
+    def index(id):
+        # Query to get actor by ID
+        del_actor = Actors.query.filter(Actors.id == id).one_or_none()
+
+        # Query to get actor's assignments
+        del_actor_assignments = Assignments.query.filter(Assignments.actor_id == id).all()
+        del_actor_assignments_list = [x.format() for x in del_actor_assignments]
+
+        for z in del_actor_assignments:
+            print(z.id)
+            print(z.actor_id)
+
+        return jsonify({
+            'actor': del_actor.id,
+            'assignment': del_actor_assignments_list,
+            'success': 200
+        }), 200
+        #return 'Looking good. Please use other endpoints.'
+
+    @app.route('/movies', methods=['GET'])
+    def get_movies():
+        try:
+            # Get all movies
+            movies = Movies.query.all()
+            movies_list = [movie.format() for movie in movies]
+
+            return jsonify({
+                'success': True,
+                'movies_list': movies_list
+            }), 200
+        except Exception:
+            abort(422)
+
+
+    @app.route('/movies', methods=['POST'])
+    def insert_movie():
+        # Get data
+        print(request.get_json())
+        new_title = request.get_json()['title']
+        new_release_date = request.get_json()['release_date']
+
+        # check movie title for duplicate
+        check_movie_title = Movies.query.filter(Movies.title.ilike('%{}%'.format(new_title))).all()
+        if len(check_movie_title) > 0:
+            raise Exception("Sorry, no duplicate movie titles allowed")
+            abort(409)
+        else:
+            try:
+                # Prepare INSERT transaction
+                new_movie = Movies(title=new_title, release_date=new_release_date)
+                new_movie.insert()
+
+                # Display latest data
+                selection = Movies.query.filter(Movies.title == new_title).first()
+                return jsonify({
+                  'id': selection.id,
+                  'title': selection.title,
+                  'release_date': selection.release_date,
+                  'success': True
+                }), 200
+
+            except Exception:
+                abort(422)
+
+    @app.route('/movies/<int:id>', methods=['DELETE'])
+    def delete_movie(id):
+        try:
+            # Query to get movie by ID
+            del_movie = Movies.query.filter(Movies.id == id).one_or_none()
+
+            if del_movie is None:
+                abort(404)
+
+            # Query to get movie's assignments for deletion
+            del_movie_assignments_list = Assignments.query.filter(Assignments.movie_id == id).all()
+
+            if len(del_movie_assignments_list) > 0:
+                for movie in del_movie_assignments:
+                    del_movie_assignment = Assignments.query.filter(Assignments.id == assignment.id).one_or_none()
+                    del_movie_assignment.delete()
+            # Execute delete transaction
+            del_movie.delete()
+
+            return jsonify({
+                'deleted': id,
+                'success': True
+            }), 200
+
+        except:
+            abort(422)
+
+    @app.route('/movies/<int:id>', methods=['PATCH'])
+    def edit_movie(id):
+        try:
+            # Query to get movie by ID
+            edit_movie = Movies.query.filter(Movies.id == id).one_or_none()
+
+            if edit_movie is None:
+                abort(404)
+
+            # Get data for editing
+            edit_title = ""
+            edit_release_date = ""
+
+            if request.get_json().get('title'):
+                edit_title = request.get_json()['title']
+
+                # check  title for duplicate
+                check_movie_title = Movies.query.filter(Movies.title.ilike('%{}%'.format(edit_title))).all()
+
+                if len(check_movie_title) > 0:
+                    raise Exception("Sorry, no duplicate movie titles allowed")
+                    abort(409)
+
+            if request.get_json().get('release_date'):
+                edit_release_date = request.get_json()['release_date']
+
+            # Make sure we only update where we have data
+            if edit_title != "":
+                edit_movie.title = edit_title
+            if edit_release_date != "":
+                edit_movie.release_date = edit_release_date
+
+            edit_movie.update()
+
+            return jsonify({
+                'id': edit_movie.id,
+                'title': edit_movie.title,
+                'release_date': edit_movie.release_date,
+                'success': True
+            }), 200
+
+        except:
+            abort(422)
+
+    @app.route('/actors', methods=['GET'])
+    def get_actors():
+        try:
+            # Get all movies
+            actors = Actors.query.all()
+            actors_list = [actor.format() for actor in actors]
+
+            return jsonify({
+            'success': True,
+            'actors_list': actors_list
+            }), 200
+        except Exception:
+            abort(422)
+
+    @app.route('/actors', methods=['POST'])
+    def insert_actor():
+        try:
+            # Get data
+            print(request.get_json())
+            new_name = request.get_json()['name']
+            new_age = request.get_json()['age']
+            new_gender = request.get_json()['gender']
+
+            # Prepare INSERT transaction
+            new_actor = Actors(name=new_name, age=new_age, gender=new_gender)
+            new_actor.insert()
+
+            # Display latest data
+            selection = Actors.query.filter(Actors.name == new_name).first()
+            return jsonify({
+            'id': selection.id,
+            'name': selection.name,
+            'age': selection.age,
+            'gender': selection.gender,
+            'success': True
+            }), 200
+
+        except Exception:
+            abort(422)
+
+    @app.route('/actors/<int:id>', methods=['DELETE'])
+    def delete_actor(id):
+        try:
+            # Query to get actor by ID
+            del_actor = Actors.query.filter(Actors.id == id).one_or_none()
+
+            if del_actor is None:
+                abort(404)
+
+            # Query to get actor's assignments for deletion
+            del_actor_assignments = Assignments.query.filter(Assignments.actor_id == id).all()
+
+            if len(del_actor_assignments) > 0:
+                for assignment in del_actor_assignments:
+                    del_assignment = Assignments.query.filter(Assignments.id == assignment.id).one_or_none()
+                    del_assignment.delete()
+
+            # Execute delete transaction
+            del_actor.delete()
+
+            return jsonify({
+                'deleted': id,
+                'success': True
+            }), 200
+
+        except:
+            abort(422)
+
+    @app.route('/actors/<int:id>', methods=['PATCH'])
+    def edit_actor(id):
+        try:
+            # Query to get actor by ID
+            edit_actor = Actors.query.filter(Actors.id == id).one_or_none()
+
+            if edit_actor is None:
+                abort(404)
+
+            # Get data for editing
+            edit_name = ""
+            edit_age = ""
+            edit_gender = ""
+
+            if request.get_json().get('name'):
+                edit_name = request.get_json()['name']
+
+            if request.get_json().get('age'):
+                edit_age = request.get_json()['age']
+
+            if request.get_json().get('gender'):
+                edit_gender = request.get_json()['gender']
+
+            # Make sure we only update where we have data
+            if edit_name != "":
+                edit_actor.name = edit_name
+            if edit_age != "":
+                edit_actor.age = edit_age
+            if edit_gender != "":
+                edit_actor.gender = edit_gender
+
+            edit_actor.update()
+
+            return jsonify({
+                'id': edit_actor.id,
+                'name': edit_actor.name,
+                'age': edit_actor.age,
+                'gender': edit_actor.gender,
+                'success': True
+            }), 200
+
+        except:
+            abort(422)
+
+    @app.route('/assignments', methods=['GET'])
+    def get_assignments():
+        try:
+            # Get all movies
+            assignments = Assignments.query.all()
+            assignments_list = [assignment.format() for assignment in assignments]
+
+            return jsonify({
+                'success': True,
+                'actors_list': assignments_list
+            }), 200
+        except Exception:
+            abort(422)
+
+    @app.route('/assignments', methods=['POST'])
+    def create_assignment():
+        try:
+            # Get data
+            new_movie_id = request.get_json()['movie_id']
+            new_actor_id = request.get_json()['actor_id']
+
+            # check movie exists
+            check_movie = Movies.query.get(new_movie_id)
+            if not check_movie:
+              raise Exception("Sorry, movie not available")
+              abort(404)
+
+            # check actor exists
+            check_actor = Actors.query.get(new_actor_id)
+            if not check_actor:
+              raise Exception("Sorry, actor not available")
+              abort(404)
+
+            # Prepare INSERT transaction
+            new_assignment = Assignments(movie_id=new_movie_id, actor_id=new_actor_id)
+            new_assignment.insert()
+
+            # Display latest data
+            selection = Assignments.query.order_by(Assignments.id.desc()).first()
+
+            return jsonify({
+              'id': selection.id,
+              'movie_id': selection.movie_id,
+              'actor_id': selection.actor_id,
+              'success': True
+            }), 200
+
+        except Exception:
+            abort(422)
+
+    @app.route('/assignments/<int:id>', methods=['DELETE'])
+    def delete_assignment(id):
+        try:
+            # Query to get assignment by ID
+            del_assignment = Assignments.query.filter(Assignments.id == id).one_or_none()
+
+            if del_assignment is None:
+                abort(404)
+
+            # Execute delete transaction
+            del_assignment.delete()
+
+            return jsonify({
+            'deleted': id,
+            'success': True
+            }), 200
+
+        except:
+            abort(422)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "Resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "Unprocessable"
+        }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad Request"
+        }), 400
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "Internal Server Error"
+        }), 500
+
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({
+        "success": False,
+        "error": 405,
+        "message": "Method Not Allowed"
+        }), 405
+
+    return app
+
+APP = create_app()
+
+if __name__ == '__main__':
+    APP.run(host='0.0.0.0', port=8080, debug=True)
---
+++ models.py
@@ -0,0 +1,141 @@
+import os
+from sqlalchemy import (
+    Column, String, Integer, Table, DateTime, ForeignKey,
+)
+from flask_sqlalchemy import SQLAlchemy
+import json
+import dateutil.parser
+import datetime
+#-------------------
+
+database_name = "casting_agency"
+database_path = "postgresql://{}/{}".format('postgres:marco@localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+'''
+Movies
+
+'''
+
+
+class Movies(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(120), unique=True, nullable=False)
+    release_date = db.Column(db.DateTime, nullable=False)
+    created_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+    assignments = db.relationship('Assignments', backref='movies', lazy=True)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+
+'''
+Actors
+
+'''
+
+
+class Actors(db.Model):
+    __tablename__ = 'actors'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(120), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(6), nullable=False)
+    created_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+    assignments = db.relationship('Assignments', backref='actors', lazy=True)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+
+'''
+Assignments
+
+'''
+
+
+class Assignments(db.Model):
+    __tablename__ = 'assignments'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    movie_id = db.Column(db.Integer, ForeignKey('movies.id'), nullable=False)
+    actor_id = db.Column(db.Integer, ForeignKey('actors.id'), nullable=False)
+    created_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+
+    def __init__(self, movie_id, actor_id):
+        self.movie_id = movie_id
+        self.actor_id = actor_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'movie_id': self.movie_id,
+            'actor_id': self.actor_id,
+        }
