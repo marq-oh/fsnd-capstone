9d8eeb011515fe8e9d769d09968e6e5bc9e365c0
marq-oh <58898977+marq-oh@users.noreply.github.com>
2020-02-16 19:59:06 -0800

    Modified database_path

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ import datetime
 #-------------------
 
 database_name = "casting_agency"
-database_path = "postgresql://{}/{}".format('postgres:marco@localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
